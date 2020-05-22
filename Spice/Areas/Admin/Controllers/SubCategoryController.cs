b2a90eee39d321b0d048891ba401ba1b68cdc6c5
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-05-22 22:32:20 +0200

    Add SubCategory Edit View and Action Methods

--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -37,7 +37,7 @@ namespace Spice.Areas.Admin.Controllers
             SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
             {
                 CategoryList = await this.db.Category.ToListAsync(),
-                SubCategory = new Models.SubCategory(),
+                SubCategory = new SubCategory(),
                 SubCategoryList = await this.db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
             };
 
@@ -49,7 +49,7 @@ namespace Spice.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(SubCategoryAndCategoryViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var doesSubCategoryExists = this.db.SubCategory.Include(s => s.Category).Where(s => s.Name == model.SubCategory.Name && s.Category.Id == model.SubCategory.CategoryId);
 
@@ -82,10 +82,72 @@ namespace Spice.Areas.Admin.Controllers
         {
             List<SubCategory> subCategories = new List<SubCategory>();
             subCategories = await (from subCategory in this.db.SubCategory
-                             where subCategory.CategoryId == id
-                             select subCategory).ToListAsync();
+                                   where subCategory.CategoryId == id
+                                   select subCategory).ToListAsync();
 
             return Json(new SelectList(subCategories, "Id", "Name"));
+        }
+
+        // GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await this.db.SubCategory.SingleOrDefaultAsync(s => s.Id == id);
+
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await this.db.Category.ToListAsync(),
+                SubCategory = subCategory,
+                SubCategoryList = await this.db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubCategoryExists = this.db.SubCategory.Include(s => s.Category).Where(s => s.Name == model.SubCategory.Name && s.Category.Id == model.SubCategory.CategoryId);
+
+                if (doesSubCategoryExists.Count() > 0)
+                {
+                    //Error
+                    StatusMessage = "Error: Sub Category already exists under " + doesSubCategoryExists.First().Category.Name + " category. Please use another name.";
+                }
+                else
+                {
+                    var subCatFromDb = await this.db.SubCategory.FindAsync(model.SubCategory.Id);
+                    subCatFromDb.Name = model.SubCategory.Name;
+
+                    await this.db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await this.db.Category.ToListAsync(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = await this.db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync(),
+                StatusMessage = StatusMessage,
+            };
+
+            //Another way of persisting an id
+            //modelVM.SubCategory.Id = id;
+            return View(modelVM);
         }
     }
 }
