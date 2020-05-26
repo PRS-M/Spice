0a560217fee902cd6740cf8d4dd79f73ef19d541
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-05-27 00:03:54 +0200

    Add Property Binding and Create Action Method

--- Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models.ViewModels;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -15,16 +16,30 @@ namespace Spice.Areas.Admin.Controllers
         private readonly ApplicationDbContext db;
         private readonly IWebHostEnvironment hostEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
         public MenuItemController(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
         {
             this.db = db;
             this.hostEnvironment = hostEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = this.db.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
 
         public async Task<IActionResult> Index()
         {
             var menuItems = await this.db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
