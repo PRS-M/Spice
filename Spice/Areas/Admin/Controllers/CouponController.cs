22a8facc5c4a268234909f8e4b4115fcf28cf942
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-06-03 00:06:35 +0200

    Create CouponController

---
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CouponController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await this.db.Coupon.ToListAsync());
+        }
+    }
+}
