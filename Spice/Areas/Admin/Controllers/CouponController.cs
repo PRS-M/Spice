e3d9b7052382399fd2be4e4644a2926b51b9c767
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-06-06 01:42:14 +0200

    Add EditPOST and Details Action Methods

--- Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -79,5 +79,64 @@ namespace Spice.Areas.Admin.Controllers
 
             return View(coupon);
         }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPOST(Coupon coupon)
+        {
+            if (coupon.Id == 0)
+            {
+                return NotFound();
+            }
+
+            var couponFromDb = await this.db.Coupon.Where(c => c.Id == coupon.Id).FirstOrDefaultAsync();
+
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+
+                    couponFromDb.Picture = p1;
+                }
+
+                couponFromDb.Name = coupon.Name;
+                couponFromDb.CouponType = coupon.CouponType;
+                couponFromDb.Discount = coupon.Discount;
+                couponFromDb.MinimumAmount = coupon.MinimumAmount;
+                couponFromDb.IsActive = coupon.IsActive;
+
+                await this.db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(coupon);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await this.db.Coupon.FindAsync(id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
     }
 }
