96fadfe85a451c50fa25f866b1fed92460fd4229
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-06-17 01:21:44 +0200

    Configure Session

--- Spice/Startup.cs
+++ Spice/Startup.cs
@@ -46,6 +46,12 @@ namespace Spice
                 options.LogoutPath = $"/Identity/Account/Logout";
                 options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
             });
+            services.AddSession(options =>
+            {
+                options.Cookie.IsEssential = true;
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,6 +70,8 @@ namespace Spice
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseSession();
 
             app.UseRouting();
 
